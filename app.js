fd2333f267eaadb8d1dd45dd82e1b78573b8e853
Hyungrae <nhr1225@naver.com>
2019-06-20 14:30:42 +0900

    1234

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const http = require('http');
+var http = require('http');
+var fs = require('fs');
+// var hostname = '127.0.0.1';
+// var port = 3000;
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var server = http.createServer(function(req, res) {
 
-const server = http.createServer((req, res) => {
+  //http://localhost/index.html : req.url  = index.html
+  // fs.readFileSync(__dirname + req.url);
+  var url = "";
+  if(req.url == "/"){
+    url = "/index.html";
+  } else if (req.url == "/favicon.ico") {
+      return res.writeHead(404);
+  } else if (require("path").extname(req.url) == ".css") {
+      url = req.url;
+      res.setHeader('Content-Type', 'text/plain');
+  } else {
+      url = req.url;
+      res.setHeader('Content-Type', 'text/plain');
+  }
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end(fs.readFileSync(__dirname + req.url));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(80, function() {
+  console.log(`Server running~`);
 });
